6aa530242f0e6888f9c64cb371de73486eaf375f
Heikki Tiainen <23316122+htiainen@users.noreply.github.com>
2021-02-28 00:29:16 +0200

    serve data on express

--- server/index.js
+++ server/index.js
@@ -3,11 +3,18 @@ const epxress = require("express");
 
 const config = require("./config.json");
 
+const server = epxress();
+
 const instance = axios.create({
     baseURL: "https://bad-api-assignment.reaktor.com/v2",
     headers: {"x-force-error-mode": config.FORCE_REQUEST_FAILURE ? "all" : "none"}
 });
 
-const resp = instance.get("products/facemasks").then(response => {
-    console.log(response.data)
+server.listen(8080, () => {console.log("server up on 8080")})
+server.get("/", (req, res) => {
+    instance.get("products/facemasks").then(response => {
+        res.send(
+            response.data
+        );
+    })
 });

8a0e8d76f07ef5ca607456a69e43adbacac3d28e
Heikki Tiainen <23316122+htiainen@users.noreply.github.com>
2021-03-01 16:01:04 +0200

    implement frontend

--- client/src/App.js
+++ client/src/App.js
@@ -2,37 +2,65 @@ import { useEffect, useState } from 'react';
 import { Link, BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
-const Home = () => {
+const Product = (props) => {
+  const {id, name, color, price, manufacturer, availability} = props.product;
+  return (
+    <tr key={id}>
+      <td>{name}</td>
+      <td>{manufacturer}</td>
+      <td>{color}</td>
+      <td>{availability}</td>
+      <td>{price}</td>
+    </tr>
+  )
+}
+
+const ProductTable = (props) => {
+  return (
+    <table>
+      <tbody>
+        {props.products.map(item => {
+          return <Product product={item} />
+        })}
+      </tbody>
+    </table>
+  )
+} 
+
+const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState();
+  const [productType, setProductType] = useState("gloves");
 
   useEffect(() => {
     fetch("http://localhost:8080/api/")
-      .then(res => {console.log(res); return res.json()}).then(response => {
-        setData(response.results);
-        console.log(response)
+      .then(res => res.json()).then(response => {
+        setData(response);
         setIsLoading(false);
       },
       e => {
         console.error(e)
       });
   }, []);
-  return (
-    <>
-      {!isLoading && <p>{data}</p>}
 
-    </>
+  return (
+    <div>
+      {!isLoading &&
+          <>
+            <nav>
+              {data.map((item, index) => {
+                const type = item[0].type;
+                return <button key={index} onClick={() => {
+                  setProductType(type);
+                }}>{type}</button>
+              })}
+            </nav>
+            <ProductTable products={data.find(item => item[0].type === productType)} />
+          </>
+      }
+    </div>
+    
   )
-}
-
-const App = () => {
-  return (
-    <>
-      <Router>
-        <Route exact path="/" component={Home} />
-      </Router>
-    </>
-  );
 }
 
 export default App;

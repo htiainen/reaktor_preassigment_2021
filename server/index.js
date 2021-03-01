7c0544cac08f1d086f491a43bf6fb53b3644e0c8
Heikki Tiainen <23316122+htiainen@users.noreply.github.com>
2021-03-01 03:45:56 +0200

    serve static files and fix a dumb typo

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
-const epxress = require("express");
+const express = require("express");
 const Cache = require("node-cache");
 
 const legacyAPIInterface = require("./legacyAPIInterface.js");
 const aggregation = require("./aggregation.js");
 
-const server = epxress()
+const server = express()
+
 const cache = new Cache({
     stdTTL: 5*60,
     checkperiod: 60,
@@ -25,8 +26,11 @@ cache.on("expired", (key, value) => {
     updateCache();
 });
 
+server.use(express.static("../client/build"));
+
 server.listen(8080, () => {console.log("server up on 8080")});
 
 server.get("/api", (req, res) => {
     res.json(cache.get("productData"));
 });
+

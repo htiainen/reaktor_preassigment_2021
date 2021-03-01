31014e74aef203533681b25c53933b54d70ba1c6
Heikki Tiainen <23316122+htiainen@users.noreply.github.com>
2021-03-01 04:54:14 +0200

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,37 @@
+import { useEffect, useState } from 'react';
+import { Link, BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
-function App() {
+const Home = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch("localhost:8080/api/", {mode: "no-cors"})
+      .then(res => {console.log(res); return res.json()}).then(response => {
+        setData(response.results);
+        console.log(response)
+        setIsLoading(false);
+      },
+      e => {
+        console.error(e)
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        pog
-      </header>
-    </div>
+    <>
+      {!isLoading && <p>{data}</p>}
+
+    </>
+  )
+}
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Route exact path="/" component={Home} />
+      </Router>
+    </>
   );
 }
 
